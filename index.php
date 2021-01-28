b1625afa0f157a00c38c58f59836f0fc4d2947e0
yann tyb <yanntyb.lbc@gmail.com>
2021-01-28 13:29:57 +0100

    a

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 // Premiere ligne
+if(isset($_POST["fname"])){
+    $name = strip_tags($_POST["fname"]);
+}
+else{
+    $name = "error";
+}
 
-
-echo "Bienvenue". ;
+echo "Bienvenue ". $_POST["fname"];
 
 
 
